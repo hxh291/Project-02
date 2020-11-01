3c27999257cf6edb44be609222db068236b80757
Aaron Tapp <tappthegreat@gmail.com>
2020-11-01 14:38:27 -0600

    if_else comments

--- if_else.sh
+++ if_else.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+#the if/else block applies a condition that verifies if a number a is less than
+#number b. If it is, it will echo whatever numa is and that it is less
+#than numb, else, it will say the opposite. 
+# It is interesting that you end the block with fi, instead of a bracket.
 num_a=400
 num_b=200
 
