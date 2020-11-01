9f6169e8739281ffbf4380b5defc467eee6a8cd8
Aaron Tapp <tappthegreat@gmail.com>
2020-11-01 15:05:38 -0600

    Until comments

--- until.sh
+++ until.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+#the until block does what a do while block does, it will 
+#keep running the code, in this case, counter =4, and until counter is less
+#3, let counter -=1 and then echo the counter. It will continue until the 
+# until is met. Some differences include the obvious done statement, to end
+# the code, as well as the use of [] for the code block instead of parenthesis.
 
 counter=4
 until [ $counter -lt 3 ]; do
